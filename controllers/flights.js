0eebc15c65aa1ac3bda9efef6633ba417364e377
aunshpatel <patelaunsh28@gmail.com>
2023-07-17 21:15:20 -0400

    Code cleanup

--- controllers/flights.js
+++ controllers/flights.js
@@ -12,8 +12,6 @@ async function index(req, res) {
 }
 
 function newFlight(req, res) {
-    // We'll want to be able to render an  
-    // errorMsg if the create action fails
     res.render('flights/new', { title: 'Add New Flight', errorMsg: '' });
 }
 
@@ -29,11 +27,8 @@ function newFlight(req, res) {
     req.body.departs = req.body.departs;
     try {
       await Flight.create(req.body);
-      // Always redirect after CUDing data
-      // We'll refactor to redirect to the movies index after we implement it
-      res.redirect('/flights');  // Update this line
+      res.redirect('/flights');
     } catch (err) {
-      // Typically some sort of validation error
       console.log(err);
       res.render('flights/new', { errorMsg: err.message });
     }
